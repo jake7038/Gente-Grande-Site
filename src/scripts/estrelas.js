83888b7d1577cd53a6091238b8258712dd907d60
rafa <rafaelcostatricolor@gmail.com>
2024-09-03 21:46:55 -0300

    versão 1.0

--- src/scripts/estrelas.js
+++ src/scripts/estrelas.js
@@ -1,6 +1,6 @@
 
 var btn = [document.querySelector("#pessoa0"), document.querySelector("#pessoa1"), document.querySelector("#pessoa2"), document.querySelector("#pessoa3"), document.querySelector("#pessoa4"), document.querySelector("#pessoa5")]
-var back = [document.querySelector(".return0"), document.querySelector(".return1"), document.querySelector(".return2"), document.querySelector(".return3"), document.querySelector(".return4"), document.querySelector(".return5")]
+var back = [document.querySelector("#return0"), document.querySelector("#return1"), document.querySelector("#return2"), document.querySelector("#return3"), document.querySelector("#return4"), document.querySelector("#return5")]
 
 for(let i = 0; i< 6; i++){
     btn[i].addEventListener("click", function(){
